edf50acb48b7ec235d09d10d2f1007088134beef
KamilVogel <81777296+KamilVogel@users.noreply.github.com>
2022-04-07 20:55:38 +0200

    added incomplete code for sending messages

--- func.js
+++ func.js
@@ -14,4 +14,62 @@
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
     });
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0){
+            return;
+        }
+        renderMessage("my",{
+            username:uname,
+            text:message
+        });
+        socket.emit("chat",{
+            username:uname,
+            text:message
+        });
+        app.querySelector(".chat-screen #messages").value = "";
+    });
+
+    socket.on("update", function(update){
+        renderMessage("update",update);
+    });
+
+    socket.on("chat", function(update){
+        renderMessage("someone",update);
+    });
+
+    function renderMessage(type,message){
+        let messageContainer = app.querySelector(".chat-screen #messages")
+        if(type == "my"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message my-message");
+            el.innerHTML = `
+            <div>
+                <div class="name">Ty</div>
+                <div class="text">${message.text}</div>
+            </div>
+            `;
+            messageContainer.appendChild(el); // Here it crashes, when sending message
+        }
+        else if(type == "someones"){
+            let el = document.createElement("div");
+            el.setAttribute("class","message someones-message");
+            el.innerHTML = `
+            <div>
+                <div class="name">${message.username}</div>
+                <div class="text">${message.text}</div>
+            </div>
+            `;
+            messageContainer.appendChild(el);
+        }
+        else if(type == "update"){
+            let el = document.createElement("div");
+            el.setAttribute("class","update");
+            el.innerText = message;
+            messageContainer.appendChild(el);
+        }
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+    }
+
 })();
